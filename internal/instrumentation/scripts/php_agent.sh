11df826d00d2e4ecdf993b6d7368e29463e6c23b
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-03 09:26:02 -0700

    Updated

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -6,10 +6,12 @@ set -x
 
 echo "PHP auto-instrumentation init container started with args: $*"
 
-#mounted_dir="$1"
-#
-#cp -r /autoinstrumentation/opentelemetry.ini "$mounted_dir"
-#cp -r /autoinstrumentation/version.txt "$mounted_dir"
+mounted_dir="$1"
+
+echo "Copying auto-instrumentation files to mounted directory: $mounted_dir"
+
+cp -r /autoinstrumentation/opentelemetry.ini "$mounted_dir"
+cp -r /autoinstrumentation/version.txt "$mounted_dir"
 #
 #extension_dir=$(php -i | grep "^extension_dir" | awk '{print $5}')
 #
