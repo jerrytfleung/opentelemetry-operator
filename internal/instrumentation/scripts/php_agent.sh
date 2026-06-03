f4345ce50dd6786dab9194548c8ae9ef9887d91e
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-03 09:45:38 -0700

    try

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -12,10 +12,15 @@ echo "Copying auto-instrumentation files to mounted directory: $mounted_dir"
 
 cp -r /autoinstrumentation/opentelemetry.ini "$mounted_dir"
 cp -r /autoinstrumentation/version.txt "$mounted_dir"
-#
-#extension_dir=$(php -i | grep "^extension_dir" | awk '{print $5}')
-#
-#api=$(php -i | grep "^PHP API => " | awk '{print $4}')
+
+extension_dir=$(php -i | grep "^extension_dir" | awk '{print $5}')
+
+echo "PHP extension directory detected: $extension_dir"
+
+api=$(php -i | grep "^PHP API => " | awk '{print $4}')
+
+echo "PHP API version detected: $api"
+
 #
 ## check if alpine
 #standard_c_lib=glibc
