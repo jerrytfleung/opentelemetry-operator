70dd225911b1cf5e6fc67993f478902da6d656bb
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-02 22:32:49 -0700

    Narrow down first

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -6,21 +6,21 @@ set -x
 
 echo "PHP auto-instrumentation init container started with args: $*"
 
-mounted_dir="$1"
-
-cp -r /autoinstrumentation/opentelemetry.ini "$mounted_dir"
-cp -r /autoinstrumentation/version.txt "$mounted_dir"
-
-extension_dir=$(php -i | grep "^extension_dir" | awk '{print $5}')
-
-api=$(php -i | grep "^PHP API => " | awk '{print $4}')
-
-# check if alpine
-standard_c_lib=glibc
-if [ -f /etc/alpine-release ]; then
-    standard_c_lib=musl
-fi
-
-cp -r /autoinstrumentation/"$api"/"$standard_c_lib"/* "$extension_dir"
+#mounted_dir="$1"
+#
+#cp -r /autoinstrumentation/opentelemetry.ini "$mounted_dir"
+#cp -r /autoinstrumentation/version.txt "$mounted_dir"
+#
+#extension_dir=$(php -i | grep "^extension_dir" | awk '{print $5}')
+#
+#api=$(php -i | grep "^PHP API => " | awk '{print $4}')
+#
+## check if alpine
+#standard_c_lib=glibc
+#if [ -f /etc/alpine-release ]; then
+#    standard_c_lib=musl
+#fi
+#
+#cp -r /autoinstrumentation/"$api"/"$standard_c_lib"/* "$extension_dir"
 
 echo "PHP auto-instrumentation init container completed successfully"
