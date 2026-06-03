13f6fb957d0da2ef8ab82dddf089c5643c94452a
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-03 13:26:03 -0700

    Modify e2e php test image

--- tests/test-e2e-apps/php/index.php
+++ tests/test-e2e-apps/php/index.php
@@ -8,8 +8,11 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $result = random_int(1,6);
-    $response->getBody()->write(strval($result));
+    $all_envs = getenv();
+    $all_values_string = implode(", ", $all_envs);
+    $response->getBody()->write(all_values_string);
+    // $result = random_int(1,6);
+    // $response->getBody()->write(strval($result));
     return $response;
 });
 
