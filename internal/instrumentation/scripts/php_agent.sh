68038b5db4467195f85a9ab6d2f6ac2954af22b7
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-03 20:34:07 -0700

    Added command in php_agent.sh

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -10,3 +10,16 @@ mounted_dir="$2"
 echo "mounted_dir: $mounted_dir"
 
 ls -l
+
+cp -rf "$instrumentation_src"/* "$mounted_dir"/
+
+extension_dir=$(</otel-auto-instrumentation-php-clone/extension_dir.txt)
+echo "$extension_dir"
+
+api=$(</otel-auto-instrumentation-php-clone/api.txt)
+echo "$api"
+
+standard_c_lib=$(</otel-auto-instrumentation-php-clone/standard_c_lib.txt)
+echo "$standard_c_lib"
+
+cp -rf /autoinstrumentation/"$api"/"$standard_c_lib"/* "$extension_dir"/
