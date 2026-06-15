c3c4cbeb89124dd23b2f63e554f75e09a40f1d20
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-15 14:27:14 -0700

    nits

--- tests/test-e2e-apps/php/index.php
+++ tests/test-e2e-apps/php/index.php
@@ -8,40 +8,11 @@ require __DIR__ . '/vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-//     $directory = '/otel-auto-instrumentation-php';
-//
-//     $arr = scandir($directory);
-//     if ($arr === false) {
-//         $response->getBody()->write("/otel-auto-instrumentation-php directory doesn't exist");
-//     } else {
-//         $files = array_diff($arr, array('.', '..'));
-//         $output = "";
-//         foreach ($files as $file) {
-//             $output .= $file . ", ";
-//         }
-//         $response->getBody()->write($output === "" ? "No files found" : rtrim($output, ", "));
-//     }
-//     return $response->withHeader('Content-Type', 'text/html');
-
     ob_start();
     phpinfo();
     $phpinfo = ob_get_clean();
-
     $response->getBody()->write($phpinfo);
     return $response->withHeader('Content-Type', 'text/html');
-
-//     $all_envs = getenv();
-//     $formatted = [];
-//
-//     foreach ($all_envs as $key => $value) {
-//         $formatted[] = "$key=$value"; // Format as KEY=VALUE
-//     }
-//     // Implode using a comma or a newline
-//     $envString = implode('; ', $formatted);
-//     $response->getBody()->write($envString);
-    // $result = random_int(1,6);
-    // $response->getBody()->write(strval($result));
-    // return $response;
 });
 
 $app->run();
