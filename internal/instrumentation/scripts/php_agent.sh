07745b998031690db3acf70402ff670250946dd0
Jerry Leung <tingfung.leung@solarwinds.com>
2026-08-19 13:45:37 -0700

    Fixed script

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -20,3 +20,5 @@ api=$(cat "$clone"/api.txt)
 standard_c_lib=$(cat "$clone"/standard_c_lib.txt)
 
 cp -rf "$instrumentation_src"/"$api"/"$standard_c_lib"/"$thread_safety"/* "$mounted_dir"/
+cp -rf "$instrumentation_src"/opentelemetry.ini "$mounted_dir"/
+cp -rf "$instrumentation_src"/version.txt "$mounted_dir"/
