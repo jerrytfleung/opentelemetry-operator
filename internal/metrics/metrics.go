11140d6a7f4924c0006640ab2ea2bc924a2b19fc
Jerry Leung <jerrytfleung@users.noreply.github.com>
2026-08-20 16:40:18 -0700

    Revert "Main 20260820 rebase (#28)"
    This reverts commit ab79d2b4b92b7d46a87671f9f6543b31520c980a.

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -169,7 +169,7 @@ func extractElements(elements map[string]any) []string {
 	itemsMap := map[string]struct{}{}
 	var items []string
 	for key := range elements {
-		itemName, _, _ := strings.Cut(key, "/")
+		itemName := strings.SplitN(key, "/", 2)[0]
 		itemsMap[itemName] = struct{}{}
 	}
 	for key := range itemsMap {
